26fe0ffd7e5ecabbe11fc3681730efad83fc6460
angelddaz <angelddaz@gmail.com>
2020-06-25 19:49:09 -0600

    Add files via upload

---
+++ rest_api_challenge/script.py
@@ -0,0 +1,11 @@
+from github import Github
+
+token = ''
+g = Github(token)
+
+import ipdb; ipdb.set_trace()
+# import pandas
+# loop through some kind of github iterable object, pulling data
+# store the data in dataframe
+# dataframe -> local csv
+# local csv -> local postgres
